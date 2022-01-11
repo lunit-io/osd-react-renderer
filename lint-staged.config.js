76ce29357c830e58adc58b6249fc4eadf99f55e8
Taiwon Chung <tchung@lunit.io>
2022-01-11 17:27:18 +0900

    feat(osd-viewer-demo): migrated osd-viewer-demo

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,35 @@
 const { ESLint } = require('eslint')
+const filterAsync = require('node-filter-async').default
 
 const eslintCli = new ESLint()
 
+const removeIgnoredFiles = async (files, eslintCli) => {
+  const filteredFiles = await filterAsync(files, async file => {
+    const isIgnored = await eslintCli.isPathIgnored(file)
+    return !isIgnored
+  })
+  return filteredFiles.join(' ')
+}
+
 module.exports = {
-  '{apps,packages}/**/*.ts?(x)': [
-    'yarn run eslint --max-warnings=0 --cache --debug',
-    'yarn run prettier --write',
-  ],
-  '{apps,packages}/**/*.js?(x)': [
-    'yarn run eslint --cache',
-    'yarn run prettier --write',
-  ],
+  '{apps,packages}/**/*.ts?(x)': async files => {
+    const filesToLint = await removeIgnoredFiles(files, eslintCli)
+    if (!filesToLint) {
+      return []
+    }
+    return [
+      `yarn run eslint --max-warnings=0 --cache --debug ${filesToLint}`,
+      `yarn run prettier --write ${filesToLint}`,
+    ]
+  },
+  '{apps,packages}/**/*.js?(x)': async files => {
+    const filesToLint = await removeIgnoredFiles(files, eslintCli)
+    if (!filesToLint) {
+      return []
+    }
+    return [
+      `yarn run eslint --cache ${filesToLint}`,
+      `yarn run prettier --write ${filesToLint}`,
+    ]
+  },
 }
