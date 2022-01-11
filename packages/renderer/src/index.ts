93f6dbf5bc853a80f57c59ce16b4831adfd45eca
Taiwon Chung <tchung@lunit.io>
2022-01-11 16:07:09 +0900

    chore(root): project set-up and renderer package migration

---
+++ packages/renderer/src/index.ts
@@ -0,0 +1,8 @@
+export { default } from './components/index'
+export { ScalebarLocation } from './types/index'
+export type {
+  ViewportProps,
+  CanvasOverlayProps,
+  TooltipOverlayProps,
+  OSDViewerRef,
+} from './types/index'
