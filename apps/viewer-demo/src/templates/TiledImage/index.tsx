0c70ad911b90d6e4db7917203a31890db66feab1
Hyunjin Kwon <hyunjin.kwon@lunit.io>
2022-01-23 21:35:37 +0900

    feat(osd-viewer-demo): implements TiledImage page

---
+++ apps/viewer-demo/src/templates/TiledImage/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { OSDViewer } from '../../components/OSDViewer'
+import Code from '../../components/Code'
+import Description from '../../components/Description'
+import Section from '../../components/Section'
+import { FULL_SOURCE, SOURCE } from './const'
+import { CODE_SANDBOX } from '../const'
+
+const VIEWER_OPTIONS = {
+  imageLoaderLimit: 8,
+  smoothTileEdgesMinZoom: Infinity,
+  showNavigator: true,
+  showNavigationControl: false,
+  timeout: 60000,
+  navigatorAutoResize: false,
+  preserveImageSizeOnResize: true,
+  showRotationControl: true,
+  zoomPerScroll: 1.3,
+  animationTime: 0.3,
+  gestureSettingsMouse: {
+    clickToZoom: false,
+    dblClickToZoom: false,
+  },
+  gestureSettingsTouch: {
+    flickEnabled: false,
+    clickToZoom: false,
+    dblClickToZoom: false,
+  },
+}
+
+export default function TiledImage(): JSX.Element {
+  return (
+    <Section>
+      <Description>
+        <OSDViewer options={VIEWER_OPTIONS}>
+          <tiledImage url="https://api.pdl1.demo.scope.lunit.io/slides/images/dzi/c76175c1-dd83-4e94-8d54-978903c753ec/16/76a4a313-3865-4232-ba26-449a664204f4/Lung_cancer_14-TPS_50-100.svs" />
+        </OSDViewer>
+      </Description>
+      <Code
+        source={SOURCE}
+        fullSource={FULL_SOURCE}
+        codesandboxUrl={CODE_SANDBOX.tiledImage}
+      />
+    </Section>
+  )
+}
---
+++ apps/viewer-demo/src/templates/const.ts
@@ -0,0 +1,4 @@
+export const CODE_SANDBOX = {
+  tiledImage:
+    'https://codesandbox.io/s/lunit-osd-react-renderer-tiledimage-ckqr2',
+}
