5edacd51d58ea30962cee04c0bbbf52ed9910a22
Tobio89 <tobio@lunit.io>
2023-01-06 12:15:41 +0900

    feat(osd-react-renderer): move svg init function to allow for late-loading elements

--- packages/renderer/src/plugins/OpenSeadragonSVGOverlay.js
+++ packages/renderer/src/plugins/OpenSeadragonSVGOverlay.js
@@ -24,6 +24,8 @@ import { SVG_NAMESPACE } from '../constants'
     this._containerWidth = 0
     this._containerHeight = 0
 
+    this._elementIsLoaded = false
+
     this._svg = document.createElementNS(SVG_NAMESPACE, 'svg')
     this._svg.style.position = 'absolute'
     this._svg.style.left = 0
@@ -35,11 +37,11 @@ import { SVG_NAMESPACE } from '../constants'
     this._node = document.createElementNS(SVG_NAMESPACE, 'g')
     this._svg.appendChild(this._node)
 
-    if (options.initializeSVGSubElements) {
-      options
-        .initializeSVGSubElements(SVG_NAMESPACE)
-        .forEach(e => this._node.appendChild(e))
-    }
+    this._initializeSVG =
+      options.initializeSVGSubElements ||
+      function () {
+        return []
+      }
 
     self.resize()
 
@@ -78,8 +80,17 @@ import { SVG_NAMESPACE } from '../constants'
     node: function () {
       return this._node
     },
-
     resize: function () {
+      if (!this._elementIsLoaded) {
+        const elems = this._initializeSVG(SVG_NAMESPACE)
+        if (elems.length) {
+          this._initializeSVG(SVG_NAMESPACE).forEach(e =>
+            this._node.appendChild(e)
+          )
+          this._elementIsLoaded = true
+        }
+      }
+
       if (this._containerWidth !== this._viewer.container.clientWidth) {
         this._containerWidth = this._viewer.container.clientWidth
         this._svg.setAttribute('width', this._containerWidth)
