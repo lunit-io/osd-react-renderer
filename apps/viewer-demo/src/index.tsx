9f2bd9160acf8dd4331ec86aae7d800cad633dea
Tobio89 <tobio@lunit.io>
2024-05-14 14:42:02 +0900

    feat(osd-viewer-demo): implement test code for osd-viewer from lib, broken

--- apps/viewer-demo/src/index.tsx
+++ apps/viewer-demo/src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+// import App from './App'
 import reportWebVitals from './reportWebVitals'
+import NewApp from './NewApp'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NewApp />
   </React.StrictMode>,
   document.getElementById('root')
 )
